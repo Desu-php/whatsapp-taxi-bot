94e456f02f9363f2451a7699831cd39868a74ae1
Bakhtiyor <teaddyson@gmail.com>
2022-10-22 18:38:05 +0500

    first commit

---
+++ App/Requests/SendMessageRequest.js
@@ -0,0 +1,20 @@
+const {check, validationResult} = require('express-validator');
+
+exports.sendMessageRequest = [
+    check('phone')
+        .notEmpty()
+        .isNumeric()
+        .bail(),
+    check('code')
+        .notEmpty()
+        .isLength({min: 4, max: 4})
+        .isNumeric()
+        .bail(),
+    (req, res, next) => {
+        const errors = validationResult(req);
+        console.log('errors', errors)
+        if (!errors.isEmpty())
+            return res.status(422).json({errors: errors.array()});
+        next();
+    },
+];
